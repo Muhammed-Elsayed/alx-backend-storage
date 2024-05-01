1dfc9b9b4ec5732e0904106516b11a7b2746c5e8
Mohamed Elsayed <mohamedelsayed652002@gmail.com>
2024-05-02 02:49:19 +0300

    3

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,6 @@
--- sql code
---
-SELECT band_name , (IFNULL(split, '2020') - formed) AS lifespan
-	FROM metal_bands WHERE style LIKE '%Glam rock%'
-	ORDER BY lifespan DESC;
+-- This SQL script select band_name, and lifespan column which is difference
+SELECT band_name, (IFNULL(split, '2020') - formed) AS lifespan
+    FROM metal_bands
+    WHERE style LIKE '%Glam rock%'
+    ORDER BY lifespan DESC;
+	
