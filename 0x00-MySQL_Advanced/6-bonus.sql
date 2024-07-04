8bb8988298c95b61f6774211984e8e9699a60ab5
Mohamed Elsayed <mohamedelsayed652002@gmail.com>
2024-07-04 23:26:08 +0300

    advanced

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -1,11 +1,18 @@
--- Script that creates a trigger that resets the attribute
--- valid_email only when the email has been changed.
-DELIMITER $$ ;
-CREATE TRIGGER validate BEFORE UPDATE ON users
-FOR EACH ROW
+-- creates a stored procedure AddBonus that adds a new correction for a student
+DROP PROCEDURE IF EXISTS AddBonus;
+DELIMITER $$
+CREATE PROCEDURE AddBonus(
+    IN user_id INT, 
+    IN project_name VARCHAR(255), 
+    IN score FLOAT)
 BEGIN
-	IF NEW.email != OLD.email THEN
-		SET NEW.valid_email = 0;
-	END IF;
-END;$$
-delimiter ;
+    DECLARE project_id INT;
+    IF (SELECT COUNT(*) FROM projects WHERE name = project_name) = 0
+    THEN
+        INSERT INTO projects (name) VALUES (project_name);
+    END IF;
+    SET project_id = (SELECT id FROM projects WHERE name = project_name LIMIT 1);
+    INSERT INTO corrections (user_id, project_id, score) VALUES(user_id, project_id, score);
+END
+$$
+DELIMITER ;
