2ac0ffcbad2eda2d6e34735ecb75a25d1bd7e789
Mohamed Elsayed <mohamedelsayed652002@gmail.com>
2024-08-03 20:12:37 +0300

    web

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,29 +1,41 @@
 #!/usr/bin/env python3
+"""In this task """
+
 
 import redis
 import requests
 from functools import wraps
-from typing import Callable
+
+r = redis.Redis()
 
 
-redis_store = redis.Redis()
-
-
-
-def data_cacher(method: Callable) -> Callable:
+def url_access_count(method):
+    """decorator for get_page function"""
     @wraps(method)
-    def invoker(url) -> str:
-        redis_store.incr(f'count:{url}')
-        result = redis_store.get(f'result:{url}')
-        if result:
-            return result.decode('utf-8')
-        result = method(url)
-        redis_store.incr(f'count:{url}')
-        redis_store.setex(f'result:{url}', 10, result)
-        return result
-    return invoker
+    def wrapper(url):
+        """wrapper function"""
+        key = "cached:" + url
+        cached_value = r.get(key)
+        if cached_value:
+            return cached_value.decode("utf-8")
+
+            # Get new content and update cache
+        key_count = "count:" + url
+        html_content = method(url)
+
+        r.incr(key_count)
+        r.set(key, html_content, ex=10)
+        r.expire(key, 10)
+        return html_content
+    return wrapper
 
 
-@data_cacher
+@url_access_count
 def get_page(url: str) -> str:
-    return requests.get(url).text
+    """obtain the HTML content of a particular"""
+    results = requests.get(url)
+    return results.text
+
+
+if __name__ == "__main__":
+    get_page('http://slowwly.robertomurray.co.uk')
